44fc05d4c6af0f8474d2bf8d5ee6496c72b1a0ca
SHIVANSH475 <158449266+SHIVANSH475@users.noreply.github.com>
2024-04-01 15:18:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ class Bot(Client):
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
             workers=200,
-            plugins={"root": "plugins"},
+            plugins={"root": "Shukla"},
             sleep_threshold=15,
         )
 
