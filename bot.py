6076f50b72c988c0ddadd888591cb290202a20fa
SHIVANSH475 <158449266+SHIVANSH475@users.noreply.github.com>
2024-04-01 15:11:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name="SHUKLA",
+            name="Snowball",
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
